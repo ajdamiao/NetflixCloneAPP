b5ae5be526bfa29b3abc5c3a27070b84afb67bc3
Damiao Junior <ajdamiaof@gmail.com>
2021-03-04 18:29:56 -0300

    Adicionado funções para cadastrar conta, fazer login, ver detalhes do filme e assistir

--- app/src/main/java/com/example/netflixclone/FormCadastro.kt
+++ app/src/main/java/com/example/netflixclone/FormCadastro.kt
@@ -2,8 +2,11 @@ package com.example.netflixclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.netflixclone.databinding.ActivityFormCadastroBinding
 import com.example.netflixclone.databinding.ActivityFormLoginBinding
+import com.google.firebase.FirebaseNetworkException
+import com.google.firebase.auth.*
 import kotlinx.android.synthetic.main.activity_form_cadastro.*
 
 class FormCadastro : AppCompatActivity() {
@@ -13,12 +16,61 @@ class FormCadastro : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        // escondi a barra com nome do app e chamei a funcao toolbar
         supportActionBar!!.hide()
+        toolBar();
 
+        binding.btCadastrar.setOnClickListener()
+        {
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+            val mensagem_erro = binding.mensagemErro
+
+            if(email.isEmpty() || senha.isEmpty())
+            {
+
+                mensagem_erro.setText("Campo de email ou senha vazio!")
+            }
+            else
+            {
+                cadastrarUsuario()
+            }
+        }
+
+    }
+
+    private fun toolBar()
+    {
         val toolbar = binding.toolbarCadastro
         toolbar.setBackgroundColor(getColor(R.color.white))
         toolbar.setNavigationIcon(getDrawable(R.drawable.ic_netflix_official_logo))
-
-
     }
+
+   private fun cadastrarUsuario()
+   {
+       val email = binding.editEmail.text.toString()
+       val senha = binding.editSenha.text.toString()
+       val mensagemErro = binding.mensagemErro
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,senha).addOnCompleteListener {
+            if(it.isSuccessful)
+            {
+                Toast.makeText(this,"Usuário cadastrado com sucesso!", Toast.LENGTH_SHORT).show()
+                binding.editEmail.setText("")
+                binding.editSenha.setText("")
+                binding.mensagemErro.setText("")
+            }
+        }.addOnFailureListener {
+
+            var erro = it;
+
+            when
+            {
+                erro is FirebaseAuthWeakPasswordException -> mensagemErro.setText("A senha conter no mínimo 6 caracteres.")
+                erro is FirebaseAuthUserCollisionException -> mensagemErro.setText("Email ja cadastrado.")
+                erro is FirebaseNetworkException -> mensagemErro.setText("Sem conexão com a internet.")
+                else -> mensagemErro.setText("Erro ao cadastrar usuário")
+            }
+        }
+   }
 }
--- app/src/main/java/com/example/netflixclone/FormLogin.kt
+++ app/src/main/java/com/example/netflixclone/FormLogin.kt
@@ -3,7 +3,11 @@ package com.example.netflixclone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.netflixclone.databinding.ActivityFormLoginBinding
+import com.google.firebase.FirebaseNetworkException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 
 class FormLogin : AppCompatActivity() {
     private lateinit var binding: ActivityFormLoginBinding
@@ -20,6 +24,61 @@ class FormLogin : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.btEntrar.setOnClickListener()
+        {
+            val email = binding.editEmail.text.toString();
+            val senha = binding.editSenha.text.toString();
+            val mensagemError = binding.mensagemErro
 
+            if (email.isEmpty() || senha.isEmpty()) {
+                mensagemError.setText("Campo de email ou senha nao preenchidos!")
+            } else {
+                autenticarUsuario()
+            }
+        }
     }
+
+    private fun autenticarUsuario()
+    {
+        val email = binding.editEmail.text.toString();
+        val senha = binding.editSenha.text.toString();
+        val mensagemError = binding.mensagemErro
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener {
+            if(it.isSuccessful)
+            {
+                Toast.makeText(this, "Login efetuado com sucesso", Toast.LENGTH_SHORT).show()
+                irParaTelaDeFilmes()
+            }
+        }.addOnFailureListener {
+
+            var erro = it;
+
+            when
+            {
+                erro is FirebaseAuthInvalidCredentialsException -> mensagemError.setText("Email ou senha incorretos.")
+                erro is FirebaseNetworkException -> mensagemError.setText("Sem conexão com a internet")
+                else -> mensagemError.setText("Erro ao fazer login.")
+            }
+        }
+    }
+
+    private fun verficiarUsuarioLogado()
+    {
+        val usuarioLogado = FirebaseAuth.getInstance().currentUser
+
+        if(usuarioLogado != null)
+        {
+            irParaTelaDeFilmes()
+        }
+    }
+
+
+    private fun irParaTelaDeFilmes()
+    {
+        val intent = Intent(this,ListaFilmes::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 }
